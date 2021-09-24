a2aaabbecfeca1e1ab69a4e62a1399383738aaa4
Adrian Hansa <adrian.hansa@yahoo.com>
2021-09-24 13:25:42 +0100

    Created Navbar

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import AboutUs from "./screens/AboutUs";
 import Login from "./screens/auth/Login";
 import Register from "./screens/auth/Register";
+import Header from "./components/Header";
 
 ReactDOM.render(
   <BrowserRouter>
+    <Header />
     <Switch>
       <Route path="/" component={App} exact />
       <Route path="/about" component={AboutUs} />
